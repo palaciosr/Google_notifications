dcbab7010df424bbbb5a38de30b7cf4942bb7934
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-09-23 15:39:56 -0700

    Changing to OOP

--- air_quality.py
+++ air_quality.py
@@ -3,3 +3,14 @@ import requests
 
 
 #for air quality 
+
+class AirQuality:
+
+    def __init__(self):
+
+        self.url = requests('/')
+
+#parse through the data 
+    def get_air_quality(self):
+
+        data = self.url.json()
