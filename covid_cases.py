c52b44837762d44a4ce7696a0bc5fa759668a9bb
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-09-23 15:39:34 -0700

    renaming and changing to OOP

---
+++ covid_cases.py
@@ -0,0 +1,19 @@
+import requests
+
+
+
+# r = requests.get(' https://corona-api.com/timeline')
+
+r= requests.get(' http://corona-api.com/countries/US')
+print(r.json())
+
+
+class CovidCases:
+
+    def __init__(self):
+        
+        self.url = requests.get('http://corona-api.com/countries/US')
+
+    def get_cases(self):
+
+       data = self.url.json()
