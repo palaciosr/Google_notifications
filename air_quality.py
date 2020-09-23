537df3786dee5bf93059f1e139dae7ecb545b2ee
Rodolfo Palacios <palaciosr080@gmail.com>
2020-09-23 12:13:06 -0700

    air quality would go here

--- air_quality.py
+++ air_quality.py
@@ -2,17 +2,4 @@ import requests
 
 
 
-#weather forecast
-print("please input the zip code of your city you would ")
-zip_code_city = input()
-appid =''
-x = 'http://api.openweathermap.org/data/2.5/weather?zip='+zip_code_city+'&units=imperial'+'&appid='+appid
-r = requests.get(x)
-r.status_code
-result = r.json()
-print(result)
-
-
-
-
-
+#for air quality 
