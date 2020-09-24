ad7926b2f175ff6c00541f37ba4f4620e5adbb5f
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-09-24 14:14:14 -0700

    Creating OOP for authenticating Google calendar and send events to calendar

--- google_auth.py
+++ google_auth.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import date, timedelta
 import pickle
 import os.path
 from googleapiclient.discovery import build
@@ -6,53 +6,69 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from httplib2 import Http
 from apiclient import discovery
+from covid_cases import CovidCases
 
-SCOPES = ['https://www.googleapis.com/auth/calendar']
-CREDENTIALS_FILE = '/Users/rodo/Downloads/credentials.json'
-def get_calendar_service():
-   creds = None
-   # The file token.pickle stores the user's access and refresh tokens, and is
-   # created automatically when the authorization flow completes for the first
-   # time.
-   if os.path.exists('token.pickle'):
-       with open('token.pickle', 'rb') as token:
-           creds = pickle.load(token)
-   # If there are no (valid) credentials available, let the user log in.
-   if not creds or not creds.valid:
-       if creds and creds.expired and creds.refresh_token:
-           creds.refresh(Request())
-       else:
-           flow = InstalledAppFlow.from_client_secrets_file(
-               CREDENTIALS_FILE, SCOPES)
-           creds = flow.run_local_server(port=0)
-       # Save the credentials for the next run
-       with open('token.pickle', 'wb') as token:
-           pickle.dump(creds, token)
-   service = build('calendar', 'v3', credentials=creds)
-   return service
-s = get_calendar_service()
+class GoogleCalendar:
+
+    def __init__(self):
+        
+
+        self.SCOPES = ['https://www.googleapis.com/auth/calendar']
+    
+        #this would be after the user allow permission to the google calendar API
+        # include site  
+        self.CREDENTIALS_FILE = '/Users/rodo/Downloads/credentials.json'
+        self.get_covid_cases = CovidCases().get_cases()
 
 
-# TIMEZONE = 'America/Los_Angeles'
-# EVENT_ID = 'primary'
-# event = {
-#     'summary': 'Appointment with people',
-#     'location': 'hell',
-#     'start': {
-#     'dateTime': '2020-09-20T10:00:00.000-07:00', #change to dynamic
-#     'timeZone': 'America/Los_Angeles'
-# },
-#     'end': {
-#     'dateTime': '2020-09-21T10:25:00.000-07:00',
-#     'timeZone': 'America/Los_Angeles'
-# },
-#     'recurrence':[
-#         'RRULE:FREQ=WEEKLY;UNTIL=20200925T170000Z'
-#     ],
-#     'attendees':[
-#         {
-#             'email':'palaciosr080@gmail.com'
-#         }
-#     ],
-# }
+    def get_calendar_service(self):
+
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    CREDENTIALS_FILE, SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('token.pickle', 'wb') as token:
+                pickle.dump(creds, token)
+        service = build('calendar', 'v3', credentials=creds)
+        return service
+
+#needs to call APIs daily 
+    def post_weather_to_calendar(self):
+
+        TIMEZONE = 'America/Los_Angeles'
+        EVENT_ID = 'primary'
+        event = {
+            'summary': 'Appointment with people',#will have weather,air quality , and covid cases 
+            'location': 'hell',
+            'start': {
+            'dateTime': date.today(),#'2020-09-20T10:00:00.000-07:00', #change to dynamic
+            'timeZone': 'America/Los_Angeles'
+        },
+            'end': {
+            'dateTime': date.today() + timedelta(5),#'2020-09-21T10:25:00.000-07:00',
+            'timeZone': 'America/Los_Angeles'
+        },
+            'recurrence':[
+                'RRULE:FREQ=WEEKLY;UNTIL=20200925T170000Z'
+            ],
+            'attendees':[
+                {
+                    'email':'palaciosr080@gmail.com'
+                }
+            ],
+        }
+
+#s=get_calendar_service()
 # recurring_event = s.events().insert(calendarId=EVENT_ID,body=event).execute()
