391e92c7315d5882dfcaf9c6dcbad4122a4d231b
Rodolfo Palacios <palaciosr080@gmail.com>
2020-09-26 17:03:57 -0700

    checking API keys

--- Api_Notifications/air_quality.py
+++ Api_Notifications/air_quality.py
@@ -5,11 +5,33 @@ import requests
 #for air quality 
 zip_code ='92706'
 date ='2020-09-23'
-api_key = ''#your API key 
+
+
+
+
+
+api_key =''#your API key 
+
+
+
 
 r = requests.get('http://www.airnowapi.org/aq/forecast/zip_code/?format=application/json&zipCode='+zip_code+'&date='+date+'&distance=50&API_KEY='+api_key)
 
 print(r.json())
+
+# data = r.json()
+# print()
+# print()
+# data['data']['name']
+
+
+
+# print(data['data']['name'])
+
+
+
+
+
 #http://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&
 # zipCode=92706&date=2020-09-23&distance=25&API_KEY=
 
