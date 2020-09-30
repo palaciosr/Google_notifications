49cab8fc9b54c98e6b491be66f3bf1fa49f9bb01
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-09-30 11:59:06 -0700

    need to fix API key check why this call is failing

--- Api_Notifications/weather_forecast.py
+++ Api_Notifications/weather_forecast.py
@@ -1,18 +1,6 @@
 import requests 
 
 
-
-#weather forecast
-print("please input the zip code of your city you would ")
-zip_code_city = input()
-appid =''
-x = 'http://api.openweathermap.org/data/2.5/weather?zip='+zip_code_city+'&units=imperial'+'&appid='+appid
-r = requests.get(x)
-r.status_code
-result = r.json()
-print(result)
-
-
 class WeatherForecast:
 
 
@@ -27,3 +15,8 @@ class WeatherForecast:
     def get_weather_forecast(self):
 
         data = self.url.json()
+
+        city_name = data['name']
+        temperature = data['main']['temp']
+
+        return city_name,temperature
