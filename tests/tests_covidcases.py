c3c96e9722da9a6995b0283ce92911fa0df8aa56
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-10-03 12:04:33 -0700

    checking the API returns 200 with native unnitest from Python

--- tests/tests_covidcases.py
+++ tests/tests_covidcases.py
@@ -1,9 +1,15 @@
 import requests
+import unittest
+
+class ApiTest(unittest.TestCase):
+
+    def test_check_api(self):
 
 
+        response =requests.get('http://corona-api.com/countries/US')
+        print(response.status_code)
+
+        self.assertEqual(response.status_code,200)
 
 
-response =requests.get('http://corona-api.com/countries/US')
-
-
-assert(response.status_code) == 200
+ApiTest().test_check_api()
