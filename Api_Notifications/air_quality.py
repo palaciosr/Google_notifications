0e589577cb2618de450031609b8cbcc61010c763
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-10-15 14:34:19 -0700

    adding API config to check API

--- Api_Notifications/air_quality.py
+++ Api_Notifications/air_quality.py
@@ -1,29 +1,22 @@
 import requests 
 from datetime import date,timedelta
 import json 
+from config_air_quality import API_KEY
 
 #for air quality 
 zip_code ='92706'
 date ='2020-09-23'
-
-
-
-
-
-api_key =''#your API key n
-
-
-
+api_key =API_KEY#your API key n
 
 #need to check that this works and returns json back
 
 r = requests.get('http://www.airnowapi.org/aq/forecast/zip_code/?format=application/json&zipCode='+zip_code+'&date='+date+'&distance=50&API_KEY='+api_key)
 
+print(r.status_code)
+print()
+
 print(r.json())
 
-# data = r.json()
-# print()
-# print()
 # data['data']['name']
 
 # print(data['data']['name'])
@@ -33,23 +26,23 @@ print(r.json())
 # zipCode=92706&date=2020-09-23&distance=25&API_KEY=
 
 
-class AirQuality:
+# class AirQuality:
 
-    pass 
+#     pass 
 
-    def __init__(self):
+#     def __init__(self):
 
-        self.zip_code = '92706'
+#         self.zip_code = '92706'
 
-        #your api key 
-        self.api_key = ''
+#         #your api key 
+#         self.api_key = ''
 
-        #today's date
-        self.date = date.today() + timedelta(1)
+#         #today's date
+#         self.date = date.today() + timedelta(1)
 
-        self.url = requests.get('http://www.airnowapi.org/aq/forecast/zip_code/?format=application/json&zipCode='+self.zip_code+'&date='+self.date+'&distance=50&API_KEY='+self.api_key)
+#         self.url = requests.get('http://www.airnowapi.org/aq/forecast/zip_code/?format=application/json&zipCode='+self.zip_code+'&date='+self.date+'&distance=50&API_KEY='+self.api_key)
 
-#parse through the data 
-    def get_air_quality(self):
+# #parse through the data 
+#     def get_air_quality(self):
 
-        data = self.url.json()
+#         data = self.url.json()
