2efbedf659beab03af48b719dbc4201e524faf70
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-10-21 14:59:03 -0700

    delete extra s from test

---
+++ tests/test_covidcases.py
@@ -0,0 +1,12 @@
+import requests
+import unittest
+
+class ApiTest(unittest.TestCase):
+
+    def test_check_api(self):
+
+
+        response =requests.get('http://corona-api.com/countries/US')
+        print(response.status_code)
+
+        self.assertEqual(response.status_code,200)
