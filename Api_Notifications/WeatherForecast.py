7aca98898d1fed0eb0fa43dddc74d6a306401d27
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-10-21 14:59:53 -0700

    Changing to camel case

---
+++ Api_Notifications/WeatherForecast.py
@@ -0,0 +1,22 @@
+import requests 
+from .config_weather_forecast import API_KEY
+
+class WeatherForecast:
+
+
+    def __init__(self):
+
+
+        self.zip_code_city = '92706'
+        self.appid = API_KEY
+
+        self.url = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+self.zip_code_city+'&units=imperial'+'&appid='+self.appid)
+
+    def get_weather_forecast(self):
+
+        data = self.url.json()
+
+        city_name = data['name']
+        temperature = data['main']['temp']
+
+        return city_name,temperature
