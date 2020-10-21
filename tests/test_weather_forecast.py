4e96ee9ff2b84f99ade98b91ef43e6a88d4a3ded
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-10-21 15:29:20 -0700

    adding test for API KEY

--- tests/test_weather_forecast.py
+++ tests/test_weather_forecast.py
@@ -1,7 +1,18 @@
 import requests
+import unittest
 from Api_Notifications.config_weather_forecast import API_KEY
 
-url = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=92706&units=imperial'+'&appid='+API_KEY)
 
+class TestWeatherForecast(unittest.TestCase):
 
-assert url.status_code == 200
+    #will pass this test if the API key is not empty
+    def test_api_key(self):
+
+        
+        self.assertTrue(API_KEY)
+
+    def test_api(self):
+
+        url = requests.get('http://api.openweathermap.org/data/2.5/weather?zip=92706&units=imperial'+'&appid='+API_KEY)
+
+        self.assertEqual(url.status_code,200)
