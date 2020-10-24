30dbc733631c442653bf8cebb15b2edb550288a7
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-10-24 11:06:11 -0700

    fixing more bugs and including edge cases

--- google_calendar.py
+++ google_calendar.py
@@ -7,7 +7,7 @@ from google.auth.transport.requests import Request
 from httplib2 import Http
 from apiclient import discovery
 from Api_Notifications.CovidCases import CovidCases
-# from Api_Notifications.air_quality import AirQuality
+from Api_Notifications.AirQuality import AirQuality
 from Api_Notifications.WeatherForecast import WeatherForecast
 
 
@@ -33,31 +33,53 @@ class GoogleCalendar:
         self.CREDENTIALS_FILE = 'credentials.json'
         self.email = recipient
 
+    def check_cred_file(self):
+
+        if not self.CREDENTIALS_FILE:
+
+            print("Go to https://developers.google.com/calendar/quickstart/python to get the credentials for the API. ")
+            return False
+        else:
+            return True
+    
+    def check_email(self):
+
+        if not self.email:
+
+            print("Needs a gmail to function")
+            return False
+        else:
+            return True
+
     def get_calendar_service(self):
         """
         The file token.pickle stores the user's access and refresh tokens, and is
         created automatically when the authorization flow completes for the first
         time.
         """
+        google_calendar = GoogleCalendar(recipient)
+        cred_api = google_calendar.check_cred_file()
+        email = google_calendar.check_email()
 
-        creds = None
+        if cred_api and email:
+            creds = None
         
-        #  User needs to safe the path locally. 
-        if os.path.exists('token.pickle'):
-            with open('token.pickle', 'rb') as token:
-                creds = pickle.load(token)
-        # If there are no (valid) credentials available, let the user log in.
-        if not creds or not creds.valid:
-            if creds and creds.expired and creds.refresh_token:
-                creds.refresh(Request())
-            else:
-                flow = InstalledAppFlow.from_client_secrets_file(
-                    self.CREDENTIALS_FILE, self.SCOPES)
-                creds = flow.run_local_server(port=0)
-            # Save the credentials for the next run
-            with open('token.pickle', 'wb') as token:
-                pickle.dump(creds, token)
-        service = build('calendar', 'v3', credentials=creds)
+            #  User needs to safe the path locally. 
+            if os.path.exists('token.pickle'):
+                with open('token.pickle', 'rb') as token:
+                    creds = pickle.load(token)
+            # If there are no (valid) credentials available, let the user log in.
+            if not creds or not creds.valid:
+                if creds and creds.expired and creds.refresh_token:
+                    creds.refresh(Request())
+                else:
+                    flow = InstalledAppFlow.from_client_secrets_file(
+                        self.CREDENTIALS_FILE, self.SCOPES)
+                    creds = flow.run_local_server(port=0)
+                # Save the credentials for the next run
+                with open('token.pickle', 'wb') as token:
+                    pickle.dump(creds, token)
+            service = build('calendar', 'v3', credentials=creds)
         return service
 
 
@@ -75,7 +97,9 @@ class GoogleCalendar:
         TIMEZONE = 'America/Los_Angeles'
 
         event = {
-            'summary': 'COVID cases'+str(cases)+ 'in the'+ str(country_name)+ 'today:' + str(updated_date)+ 'number of deaths: '+str(deaths) +'the weather today is :' +str(temperature) + 'in'+str(city_name),
+            'summary': 'COVID cases'+str(cases)+ 'in the'+ str(country_name)+ 'today:' + str(updated_date)+ 
+            'number of deaths: '+str(deaths) +'the weather today is :' +str(temperature) +
+            'in'+str(city_name)+ 'and the air quality is' +str(get_air_quality),
             'location': 'hell',
             'start': {
             'dateTime': date.today(),
@@ -98,7 +122,7 @@ class GoogleCalendar:
         return event
 
 #needs to call APIs daily 
-    def post_to_calendar(self,event):
+    def post_to_calendar(self,service,event):
 
         """
         This method finally checks that the Google account is authenticated
@@ -106,8 +130,6 @@ class GoogleCalendar:
         """
 
         EVENT_ID = 'primary'
-
-        service=GoogleCalendar().get_calendar_service()
 
         if service:
 
@@ -118,13 +140,15 @@ class GoogleCalendar:
 
 if __name__ == '__main__':
 
-    google_calendar = GoogleCalendar('email')
+    recipient = 'gmail.com'
+    google_calendar = GoogleCalendar(recipient)
+    service = google_calendar.get_calendar_service()
 
     country_name,updated_date,cases,deaths = CovidCases().get_cases()
-    # self.get_air_quality = AirQuality().get_air_quality()
+    get_air_quality = AirQuality().get_air_quality()
     city_name,temperature = WeatherForecast().get_weather_forecast()
 
-    event = google_calendar.event(country_name,updated_date,cases,deaths,city_name,temperature)
+    event = google_calendar.event(country_name,updated_date,cases,deaths,city_name,temperature,get_air_quality)
 
     #Sending to event API responses for COVID, air quality, and weather forecast.
-    GoogleCalendar().post_to_calendar(event)
+    # google_calendar.post_to_calendar(service,event)
