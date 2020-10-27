a7307e6fd55d2057a1a575135054d4471a5a80dd
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-10-27 09:17:09 -0700

    importing more tests need to resolve import problem

--- manage_tests.py
+++ manage_tests.py
@@ -1,4 +1,7 @@
 from tests.test_covidcases import ApiTest
+# from tests.test_air_quality import TestAirQuality
+# from tests.test_weather_forecast import TestWeatherForecast
+
 
 #check the API for covid cases works 
 x = ApiTest()
