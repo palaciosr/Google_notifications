754dda11a5be3722dc6a73a73515b850e52af8f2
palaciosr <36349034+palaciosr@users.noreply.github.com>
2020-10-27 09:19:48 -0700

    adding init file so python can recognize module as a package

---
+++ Api_Notifications/__init__.py
@@ -0,0 +1,6 @@
+from . import AirQuality
+from . import CovidCases
+from . import WeatherForecast
+
+from Api_Notifications.config_weather_forecast import API_KEY
+from Api_Notifications.config_air_quality import API_KEY
